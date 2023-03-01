0edb706a98a2a5bd95328775ee39faba849ab2d3
suleman <iam.suleman.m@gmail.com>
2023-03-01 18:25:10 +0500

    components checking

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import { useState } from "react";
+// import Home from '../Pages/Home';
+function Navbar() {
+    return (
+        <>
+        <nav>
+            <h2>Hello</h2>
+            <div>
+                <input type="text" />
+                <button>search</button>
+            </div>
+            <button>Home</button>
+            <button>About</button>
+            <button>contect</button>
+            <button>Login</button>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
