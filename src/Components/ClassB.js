d0ac48757628ba7a62c09b72c9fb9a03c553566f
suleman <iam.suleman.m@gmail.com>
2023-03-01 21:21:41 +0500

    macking state

--- src/Components/ClassB.js
+++ src/Components/ClassB.js
@@ -2,15 +2,20 @@ import React,{Component} from 'react';
 
 class ClassB extends Component {
 
-    constructor (props){
-        super(props);
-        this.props=props;
+    constructor (state){
+        super(state);
+        this.props= state;
+        this.state={
+            names : "abdul rehman",
+            age : 5
+        }
     }
     
     render() { 
         return ( 
             <>
                 <h1>{this.props.names} {this.props.age}</h1>
+                <h1>{this.state.names} {this.state.age}</h1>
             </>
         );
     }
