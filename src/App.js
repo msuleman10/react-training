e68761ea4735fc3ffec19e031260d9ebd3b10b40
suleman <iam.suleman.m@gmail.com>
2023-03-09 20:52:28 +0500

    making small ptroject with useState hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
 import './App.css';
-import Hello from "./Components/Card";
-
+import Head from "./Pages/Header";
+import {useState} from 'react';
 function App() {
-  let data=[
-    {
-      'mname' : "Your Name",
-      'mpath' : "https://upload.wikimedia.org/wikipedia/it/3/3b/Your_Name.jpg",
-      "mdata" : "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been thise industr"
-    },
-    {
-      'mname' : "Your Name",
-      'mpath' : "https://upload.wikimedia.org/wikipedia/it/3/3b/Your_Name.jpg",
-      "mdata" : "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been thise industr"
-    },
-    {
-      'mname' : "Your Name",
-      'mpath' : "https://upload.wikimedia.org/wikipedia/it/3/3b/Your_Name.jpg",
-      "mdata" : "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been thise industr"
-    }
-    
-];
 
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [data,setData] = useState([]);
+  
+  const add=()=>{
+    setData([...data,{name,email}]);
+    setName("");
+    setEmail("");
+  }
+  const removeData=(i)=>{
+    let arr = data;
+    arr.splice(i,1);
+    setData([...arr]);
+  }
   return (
-    <>
     <div className="app">
-      {data.map((user,index)=>(
-        <Hello key={index} name={user.mname} path={user.mpath} data={user.mdata}/>
-      ))}
+      <Head />
+      <div className='dataFetch'>
+        <div className='faields'>
+
+          <input 
+            type="text" 
+            placeholder='Name'
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+          />
+
+          <input 
+            type="email" 
+            placeholder='Email'
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+
+        </div>
+        <button onClick={add}>Add Data</button>
+      </div>
+      {data.map((e , i)=>{
+        return(
+          <div className='user-data' key={i}>
+            <h3>{e.name}</h3>
+            <h3>{e.email}</h3>
+            <button onClick={()=>removeData(i)}>Delete</button>
+          </div>
+        )
+      })}
     </div>
-    </>
   );
 }
 
---
+++ src/Pages/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div className="head">
+            <h1>User Data</h1>
+        </div>
+    )
+}
+
+export default Header
